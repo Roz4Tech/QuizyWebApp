b4240ddb06c4b4598a2030db5501ea0d3b910198
Rose Agyei <rose44agyei@gmail.com>
2025-09-17 23:11:55 +0100

    Update quizapp.py

--- quizapp.py
+++ quizapp.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*-
+# Filename: quizapp.py
+# Author: Osei- Agyei Rosemond Sarpongmaa (Rose Agyei)
+#Collaborators:Tetteh Wayo Willhermina,Sallah Prince Selasi, Agbenyegah Gabriel, Agbaglo Romeo
+# Date: 10-08-2025
+
+"""
+Project: Quizy Pop Quiz (Quiz Web Application Project)
+A modern, responsive web-based quiz application built with Python and Flask.
+Test your knowledge across various topics including general knowledge, sports,history,
+geography, and programming with an intuitive interface and comprehensive feedback system.
+"""
+
 import os
 import random
 import time
